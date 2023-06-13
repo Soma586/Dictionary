b7e8ff072704ea67e05a7e3a8f8ceeb0121e6770
Sabo <sabo@Sabos-MBP.home>
2023-06-12 20:17:05 -0400

    added in fonts and colors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './assets/fonts/inter/static/Inter-Regular.ttf'
+import './assets/fonts/lora/static/Lora-Regular.ttf'
+import './assets/fonts/inconsolata/static/Inconsolata-Regular.ttf'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
